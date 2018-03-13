e5f12139d319dd1579b781916c0a9c7143de19c3
ysuo <944102712@qq.com>
2018-03-13 18:02:07 +0800

    move files to folder

--- 1.linked_list/functional-linked-list.py
+++ 1.linked_list/functional-linked-list.py
@@ -45,12 +45,13 @@ def tail(xs):
     return xs[1]
 
 
-assert tail(lst(1, 2, 3, 4)) == lst(2,3,4)
+assert tail(lst(1, 2, 3, 4)) == lst(2, 3, 4)
 
 
 # is_empty returns True if the list contains zero elements.
 def is_empty(xs):
     return xs is Nil
+
 
 assert is_empty(Nil)
 assert not is_empty(lst(1))
@@ -63,31 +64,38 @@ def length(xs):
     else:
         return 1 + length(tail(xs))
 
+
 assert length(Nil) == 0
-assert length(lst(1,2)) == 2
+assert length(lst(1, 2)) == 2
 
 
 # concat takes two lists as arguments and concatenates them.
 # The result of concat(xs, ys) is a new list that contains all elements in xs followed by all elements in ys.
 # We implement the function with a simple divide and conquer algorithm.
 # hard
-def concat(xs1,xs2):
+def concat(xs1, xs2):
     if is_empty(xs1):
         return xs2
     else:
-        return cons(head(xs1),concat(tail(xs1),xs2))
-assert concat(lst(1,2,3),Nil) == lst(1,2,3)
-assert concat(Nil,Nil) == Nil
-assert concat(Nil,lst(1,2,3)) == lst(1,2,3)
-assert concat(lst(1,2,3),lst(4)) == lst(1,2,3,4)
+        return cons(head(xs1), concat(tail(xs1), xs2))
+
+
+assert concat(lst(1, 2, 3), Nil) == lst(1, 2, 3)
+assert concat(Nil, Nil) == Nil
+assert concat(Nil, lst(1, 2, 3)) == lst(1, 2, 3)
+assert concat(lst(1, 2, 3), lst(4)) == lst(1, 2, 3, 4)
 
 # last returns the last element of a non-empty list
+
+
 def last(xs):
     if is_empty(tail(xs)):
         return head(xs)
     else:
         return last(tail(xs))
-assert last(lst(1,2,3)) == 3
+
+
+assert last(lst(1, 2, 3)) == 3
 
 
 # init returns all elements except the last one (the initial elements).
@@ -95,75 +103,104 @@ def init(xs):
     if length(xs) == 1:
         return Nil
     else:
-        return cons(head(xs),init(tail(xs)))
-assert init(lst(1,2,3)) == lst(1,2)
+        return cons(head(xs), init(tail(xs)))
+
+
+assert init(lst(1, 2, 3)) == lst(1, 2)
 assert init(lst(1)) == Nil
+
 
 def init2(xs):
     if is_empty(tail(tail(xs))):
         return cons(head(xs))
     else:
         return cons(head(xs), init2(tail(xs)))
-assert init2(lst(1,2,3)) == lst(1,2)
+
+
+assert init2(lst(1, 2, 3)) == lst(1, 2)
 
 # The reverse function below implements list reversa
+
+
 def reverse(xs):
     if xs is Nil:
         return Nil
     elif length(xs) == 1:
         return xs
     else:
-        return cons(last(xs),reverse(init(xs)))
-assert reverse(lst(1,2,3,4)) == lst(4,3,2,1)
+        return cons(last(xs), reverse(init(xs)))
+
+
+assert reverse(lst(1, 2, 3, 4)) == lst(4, 3, 2, 1)
 assert reverse(Nil) == Nil
 assert reverse(lst(0)) == lst(0)
-assert reverse(reverse(lst(1,2,3,4))) == lst(1,2,3,4)
+assert reverse(reverse(lst(1, 2, 3, 4))) == lst(1, 2, 3, 4)
 
 # take and drop generalize head and tail by returning arbitrary prefixes and suffixes of a list.
 # For example, take(2, xs) returns the first two elements of the list xs
 # whereas drop(3, xs) returns everything except the last three elements in xs.
-def take(n,xs):
+
+
+def take(n, xs):
     if n == 0:
         return Nil
     else:
-        return cons(head(xs),take(n-1,tail(xs)))
-assert take(2,lst(1,2,3)) == lst(1,2)
+        return cons(head(xs), take(n - 1, tail(xs)))
 
-def drop(n,xs):
+
+assert take(2, lst(1, 2, 3)) == lst(1, 2)
+
+
+def drop(n, xs):
     if n == 0:
         return xs
     else:
-        return drop(n-1,tail(xs))
-assert drop(2,lst(1,2,3)) == lst(3)
-assert drop(1,lst(1,2,3)) == lst(2,3)
+        return drop(n - 1, tail(xs))
 
-#element selection
-def apply(i,xs):
-    return head(drop(i,xs))
-assert apply(0,lst(1,2,3,4)) == 1
-assert apply(2,lst(1,2,3,4)) == 3
+
+assert drop(2, lst(1, 2, 3)) == lst(3)
+assert drop(1, lst(1, 2, 3)) == lst(2, 3)
+
+# element selection
+
+
+def apply(i, xs):
+    return head(drop(i, xs))
+
+
+assert (0)(*lst(1, 2, 3, 4)) == 1
+assert (2)(*lst(1, 2, 3, 4)) == 3
 
 # insertion sort using head() tail() and is_empty
 # insert into a ordered list
-def insert(x,xs):
+
+
+def insert(x, xs):
     if is_empty(xs) or x < head(xs):
-        return cons(x,xs)
+        return cons(x, xs)
     else:
-        return cons(head(xs),insert(x,tail(xs)))
-assert insert(0,lst(1,2,3,4)) == lst(0,1,2,3,4)
-assert insert(5,lst(1,2,3,4)) == lst(1,2,3,4,5)
-assert insert(3,lst(1,2,4)) == lst(1,2,3,4)
+        return cons(head(xs), insert(x, tail(xs)))
+
+
+assert insert(0, lst(1, 2, 3, 4)) == lst(0, 1, 2, 3, 4)
+assert insert(5, lst(1, 2, 3, 4)) == lst(1, 2, 3, 4, 5)
+assert insert(3, lst(1, 2, 4)) == lst(1, 2, 3, 4)
+
 
 def isort(xs):
     if is_empty(xs):
         return xs
     else:
-        return insert(head(xs),isort(tail(xs)))
-assert isort(lst(1,2,3,4)) == lst(1,2,3,4)
-assert isort(lst(3,2,1,4)) == lst(1,2,3,4)
+        return insert(head(xs), isort(tail(xs)))
+
+
+assert isort(lst(1, 2, 3, 4)) == lst(1, 2, 3, 4)
+assert isort(lst(3, 2, 1, 4)) == lst(1, 2, 3, 4)
 
 # to_string operation returns a python style string
-def to_string(xs,prefix="[",postfix="]"):
+
+
+def to_string(xs, prefix="[", postfix="]"):
     def to_string(xs):
         if is_empty(xs):
             return ""
@@ -172,4 +209,6 @@ def to_string(xs,prefix="[",postfix="]"):
         else:
             return str(head(xs)) + "," + to_string(tail(xs))
     return prefix + to_string(xs) + postfix
-assert to_string(lst(1,2,3,4)) == "[1,2,3,4]"
+
+
+assert to_string(lst(1, 2, 3, 4)) == "[1,2,3,4]"
--- 3.coroutine/Coroutine.py
+++ 3.coroutine/Coroutine.py
@@ -1,32 +1,40 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-print "fab_1-------"
+print("fab_1-------")
+
+
 def fab_1(max):
-    n,a,b = 0,1,1
+    n, a, b = 0, 1, 1
     while n < max:
-        print a
-        a,b = b,a+b
+        print(a)
+        a, b = b, a + b
         n = n + 1
 
+
 fab_1(5)
-print "fab_2-------"
+print("fab_2-------")
+
+
 def fab_2(max):
-    n,a,b = 0,1,1
+    n, a, b = 0, 1, 1
     L = []
     while n < max:
         L.append(a)
-        a,b = b,a+b
+        a, b = b, a + b
         n = n + 1
     return L
 
+
 for i in fab_2(5):
-    print i
-print "fab_3-------"
+    print(i)
+print("fab_3-------")
+
+
 class Fab_3(object):
 
-    def __init__(self,max):
+    def __init__(self, max):
         self.max = max
-        self.n,self.a,self.b = 0,1,1
+        self.n, self.a, self.b = 0, 1, 1
 
     def __iter__(self):
         return self
@@ -34,58 +42,68 @@ class Fab_3(object):
     def next(self):
         if self.n < self.max:
             r = self.a
-            self.a,self.b = self.b,self.a+self.b
+            self.a, self.b = self.b, self.a + self.b
             self.n = self.n + 1
             return r
         raise StopIteration
 
+
 for i in Fab_3(5):
-    print i
-print "fab_4-------"
+    print(i)
+print("fab_4-------")
+
+
 def fab_4(max):
-    n,a,b = 0,1,1
+    n, a, b = 0, 1, 1
     while n < max:
         yield a
-        a,b = b,a+b
+        a, b = b, a + b
         n = n + 1
 
+
 for i in fab_4(5):
-    print i
+    print(i)
 f = fab_4(5)
-print f.next()
-print f.next()
+print(f.next())
+print(f.next())
 
-from inspect import isgeneratorfunction,isgenerator
-print isgenerator(f)
-print isgeneratorfunction(fab_4)
+from inspect import isgeneratorfunction, isgenerator
+print(isgenerator(f))
+print(isgeneratorfunction(fab_4))
 
-print "协程-生产者消费者模型------"
+print("协程-生产者消费者模型------")
+
+
 def consumer():
     r = ""
     while True:
         n = yield r
         if not n:
             return
-        print "[consumer] consuming %s" % n
+        print("[consumer] consuming %s" % n)
         r = "200 ok"
+
 
 def producer(consumer):
     consumer.send(None)
     n = 0
-    while n<5:
+    while n < 5:
         n = n + 1
-        print "[producer] producing %s" % n
+        print("[producer] producing %s" % n)
         r = consumer.send(n)
-        print "[consumser] returns: %s" % r
+        print("[consumser] returns: %s" % r)
     consumer.close()
+
 
 c = consumer()
 producer(c)
 
-print """receive = yield value 原本是执行g.next()时会执行到这一步，
+print("""receive = yield value 原本是执行g.next()时会执行到这一步，
 然后返回value的值，这里多出了一个receive，就是说，允许传一个值进来，当做此时的参数，
 此时不用next触发，而是用send触发。我觉得，用g.next()和g.send(None)是一个效果的。
-可以先给一个g.next()来启动一下，到第一次yield处------"""
+可以先给一个g.next()来启动一下，到第一次yield处------""")
+
+
 def gen():
     value = 0
     while True:
@@ -93,6 +111,8 @@ def gen():
         if receive == 'e':
             break
         value = 'got %s' % receive
+
+
 g = gen()
 print (g.next())
 print (g.next())
